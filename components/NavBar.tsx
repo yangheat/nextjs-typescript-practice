f41f91bcd32700a5a820d2ff85e8db53249acaf8
sunpark <yangheat@gmail.com>
2022-07-03 16:09:16 +0900

    Edit Navbar component style

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,46 +1,44 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-// import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      {/* <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
-      </Link>
-      <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            router.pathname === "/about" ? styles.active : "",
-          ].join(" ")}
-        >
-          About
-        </a>
-      </Link> */}
-
-      <Link href="/">
-        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-      </Link>
+      <img src="vercel.svg" alt="No img" />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
       <style jsx>{`
         nav {
-          background-color: tomato;
+          display: flex;
+          flex-direction: column;
+          gap: 10px;
+          align-items: center;
+          padding: 20px 0px 10px 0px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px;
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        a {
-          text-decoration: none;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: yellow;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
