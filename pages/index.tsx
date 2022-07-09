50b40f01f2fdd3160ec104a62c89ef0f26f493bb
sunpark <yangheat@gmail.com>
2022-07-09 22:12:53 +0900

    change style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 interface IApiMovieProps {
@@ -34,14 +35,40 @@ export default function Home() {
   }, []);
 
   return (
-    <>
+    <div className="container">
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie: IApiMovieProps) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt="No Image"
+          />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
-    </>
+
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          // border-radis: 12px;
+          // transition: transform 0.2s ease-in-out;
+          // box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
   );
 }
